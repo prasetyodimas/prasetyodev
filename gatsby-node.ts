cdd45b8ff8cc4c2dcacd0b3beec7f1fbaff91988
Dimas Prasetyo <dimasprasetyo485@gmail.com>
2023-09-24 17:42:26 +0700

    feat: init-core ui

---
+++ gatsby-node.ts
@@ -0,0 +1,13 @@
+import { CreateWebpackConfigArgs } from "gatsby";
+import * as path from "path"
+ 
+export const onCreateWebpackConfig = ({ actions }: CreateWebpackConfigArgs) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        "@/components": path.resolve(__dirname, "src/components"),
+        "@/lib/utils": path.resolve(__dirname, "src/lib/utils"),
+      },
+    },
+  })
+}
