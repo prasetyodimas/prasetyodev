23cfa1ca8f9bd0553b4413b48361f04042649f19
Dimas Prasetyo <dimasprasetyo485@gmail.com>
2023-11-02 08:28:56 +0700

    feat: create new feature of icon timelines

--- src/components/timelines.js
+++ src/components/timelines.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react"
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css'
+import Icon from '../components/icon';
 
 const Timelines = () => {
   return (
@@ -8,11 +9,10 @@ const Timelines = () => {
       <VerticalTimeline>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          contentStyle={{ background: '#f7f7f7', color: '#000' }}
+          contentArrowStyle={{ borderRight: '7px solid  #f7f7f7' }}
           date="2022 - present"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          // icon={<WorkIcon />}
+          iconStyle={{ background: 'rgb(240 240 240)', color: '#fff' }}
         >
           <h3 className="vertical-timeline-element-title">Frontend Lead</h3>
           <h4 className="vertical-timeline-element-subtitle">PT Sahabat Bisnis Inovasi</h4>
@@ -23,8 +23,8 @@ const Timelines = () => {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="2021 - 2022"
-          iconStyle={{ background: 'rgb(33 195 243)', color: '#ddd' }}
-          // icon={<WorkIcon />}
+          contentStyle={{ background: '#f7f7f7', color: '#000' }}
+          iconStyle={{ background: 'rgb(240 240 240)', color: '#fff' }}
         >
           <h3 className="vertical-timeline-element-title">Sr. Frontend Developer</h3>
           <h4 className="vertical-timeline-element-subtitle">PT Sahabat Bisnis Inovasi</h4>
@@ -35,8 +35,8 @@ const Timelines = () => {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="2018 - 2021"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#ddd' }}
-          // icon={<WorkIcon />}
+          contentStyle={{ background: '#f7f7f7', color: '#000' }}
+          iconStyle={{ background: 'rgb(240 240 240)', color: '#fff' }}
         >
           <h3 className="vertical-timeline-element-title">Frontend Developer</h3>
           <h4 className="vertical-timeline-element-subtitle">PT Pricebook Digital Indonesia</h4>
@@ -47,11 +47,11 @@ const Timelines = () => {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="2017 - 2018"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#ddd' }}
-          // icon={<WorkIcon />}
+          contentStyle={{ background: '#f7f7f7', color: '#000' }}
+          iconStyle={{ background: 'rgb(240 240 240)', color: '#fff' }}
         >
           <h3 className="vertical-timeline-element-title">Jr. Frontend Developer</h3>
-          <h4 className="vertical-timeline-element-subtitle">Amtek Labs ( Software House / IT Consultant)</h4>
+          <h4 className="vertical-timeline-element-subtitle">Amtek Labs ( Software House / IT Consultant )</h4>
           <p>
             Bootstrap · Laravel · Google Material Design · Vue · Laravel · jQuery · PostgreSQL
           </p>
@@ -59,13 +59,13 @@ const Timelines = () => {
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
           date="2011 - 2017"
-          iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-          // icon={<SchoolIcon />}
+          contentStyle={{ background: '#f7f7f7', color: '#000' }}
+          iconStyle={{ background: 'rgb(240 240 240)', color: '#fff' }}
         >
-          <h3 className="vertical-timeline-element-title">Diploma of Degree in Information System</h3>
-          <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
+          <h3 className="vertical-timeline-element-title">Information System</h3>
+          <h4 className="vertical-timeline-element-subtitle">Diploma of Degree</h4>
           <p>
-            Technology Of Yogyakarta University - D3 System Information / Informatic Management
+            Technology Of Yogyakarta University - D3 System Information / Informatic Management - 2.79
           </p>
         </VerticalTimelineElement>
       </VerticalTimeline>
