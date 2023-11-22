fb0b20c61f5342dd8450e3465325299a27160c9c
Dimas Prasetyo <dimasprasetyo485@gmail.com>
2023-11-22 17:21:43 +0700

    refactor: create dynamic css prepocessor with vendorizer

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,6 @@
 import "@fontsource-variable/montserrat"
 import "@fontsource/merriweather"
 import '@fontsource-variable/overpass-mono';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import "prismjs/themes/prism.css"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
