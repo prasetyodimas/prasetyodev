f7da18af2e5d577a8b85474a91f755c8fcf0785b
WDTI0\dimas.prasetyo <dimas.prasetyo@primecash.co.id>
2025-12-02 12:11:37 +0700

    feat: add seo jsonLd data.

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -23,10 +23,7 @@ const Buttons = ({ children, propClass, propRedirect }) => {
 }
 
 Buttons.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.node,
-    PropTypes.string,
-  ]),
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
   propClass: PropTypes.string,
   propRedirect: PropTypes.string,
 }
